cce9e76f83bc99fbf349b51c97983a02f6afb2a4
李思朋 <lisipeng@lisipengdeMacBook-Pro.local>
2019-06-13 09:00:27 +0800

    重新生成keys

--- app/components/page/HomePage.js
+++ app/components/page/HomePage.js
@@ -1,13 +1,13 @@
-import React, {Component} from 'react';
-import {
-  View, Text
-} from 'react-native';
+import React, { Component } from 'react'
+import { View, Text } from 'react-native'
 
 // 首页
 export default class HomePage extends Component {
   render() {
-    return <View>
-    <Text>HomePage</Text>
-  </View>
+    return (
+      <View>
+        <Text>HomePage</Text>
+      </View>
+    )
   }
 }
